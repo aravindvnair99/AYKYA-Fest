75fcc90e77aafbb9471a2e927f8569c2645de35f
VinayakTekade <vinayak.tekade@gmail.com>
2020-09-15 23:42:39 +0530

    Added routes for templates
    Signed-off-by: VinayakTekade <vinayak.tekade@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -30,11 +30,53 @@ app.get("/offline", (req, res) => {
 				= template routes =
 
 ===============================================>>>>>*/
-app.get("/template/", (req, res) => {
-	res.render("template/");
+app.get("/template/404", (req, res) => {
+	res.render("template/404");
 });
-app.get("/template/", (req, res) => {
-	res.render("template/");
+app.get("/template/blog", (req, res) => {
+	res.render("template/blog");
+});
+app.get("/template/blogSingle", (req, res) => {
+	res.render("template/blogSingle");
+});
+app.get("/template/contact", (req, res) => {
+	res.render("template/contact");
+});
+app.get("/template/creativeAgency", (req, res) => {
+	res.render("template/creativeAgency");
+});
+app.get("/template/index", (req, res) => {
+	res.render("template/index");
+});
+app.get("/template/index2", (req, res) => {
+	res.render("template/index2");
+});
+app.get("/template/portfolio", (req, res) => {
+	res.render("template/portfolio");
+});
+app.get("/template/portfolioSingle", (req, res) => {
+	res.render("template/portfolioSingle");
+});
+app.get("/template/schedule", (req, res) => {
+	res.render("template/schedule");
+});
+app.get("/template/speaker1", (req, res) => {
+	res.render("template/speaker1");
+});
+app.get("/template/speaker2", (req, res) => {
+	res.render("template/speaker2");
+});
+app.get("/template/venue", (req, res) => {
+	res.render("template/venue");
+});
+app.get("/template/errors/401", (req, res) => {
+	res.render("template/errors/401");
+});
+app.get("/template/errors/404", (req, res) => {
+	res.render("template/errors/404");
+});
+app.get("/template/errors/500", (req, res) => {
+	res.render("template/errors/500");
 });
 
 /*=============================================>>>>>
