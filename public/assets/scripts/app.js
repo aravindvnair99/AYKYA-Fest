8dc84b8dddb3e05cc2e32d73dc995452db25ed1b
Subaharish <48942935+Subaharish@users.noreply.github.com>
2020-09-28 15:30:28 +0530

    Added svg image in code format
    Signed-off-by: Subaharish <48942935+Subaharish@users.noreply.github.com>

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -637,7 +637,7 @@ $(document).ready(function () {
 			hour = minute * 60,
 			day = hour * 24;
 
-		let countDown = new Date("Sep 28, 2020 10:16:00").getTime(),
+		let countDown = new Date("Sep 28, 2020 15:26:00").getTime(),
 			x = setInterval(function () {
 				let now = new Date().getTime(),
 					distance = countDown - now;
